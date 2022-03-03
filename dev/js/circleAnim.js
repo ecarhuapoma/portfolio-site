2238ab86d101839931943104076be03ec08cb586
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2022-03-03 14:03:13 -0500

    still couldnt get it

---
+++ dev/js/circleAnim.js
@@ -0,0 +1,6 @@
+import { gsap } from "gsap";
+
+
+// export const circleAni = new gsap.timeline({paused:true});
+
+// circleAni.to(".bottomRound",{alpha:0});
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,24 +1,18 @@
 import { gsap } from "gsap";
+// import { circleAni } from "./circleAnim";
 
-const mainTL = new gsap.timeline();
+// const mainTL = new gsap.timeline();
 
 // window.addEventListener('load', menuListener);
 window.addEventListener('click', imageRoll);
 
 
-
-//'visibility: hidden' causes screen to open again
-// these functions work when 'visibility: hidden' is not in _header.scss
-
-gsap.set("#section-images",{alpha:0});
-gsap.set("#section-images li",{alpha:0,x:"+=20"});
-
-// function menuListener(){
-
 // }
 
 function imageRoll(){
-    
-    mainTL.to("#section-images",{alpha:1});
-    mainTL.to("#section-images li",{alpha:1,stagger:.2,duration:1,ease:"power3.out",x:0});
+
+    const rollIn = new gsap.timeline({paused:true});
+
+    rollIn.to("#section-images",{alpha:0});
+    rollIn.to("#section-images li",{alpha:1,stagger:.2,duration:1,ease:"power3.out",x:0});
 }
