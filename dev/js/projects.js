a14b7a1f73cabce9444546146da60eb8b9f9fda6
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2022-03-07 19:56:24 -0500

    god

---
+++ dev/js/projects.js
@@ -0,0 +1,17 @@
+export let projects = [
+    {
+        title:"Turtles",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis maximus arcu mattis fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam interdum eleifend commodo. Morbi pretium egestas tincidunt. Nunc porta risus vitae nunc facilisis consequat. Integer justo ipsum, lacinia non vestibulum vel, tincidunt ut augue. Nullam elementum nunc a ultricies viverra. Duis malesuada massa eros, a euismod mauris bibendum non. Curabitur tellus erat, posuere ac leo ac, pellentesque consectetur turpis. Mauris in blandit nibh, sit amet condimentum eros. Mauris ultrices orci a ipsum hendrerit efficitur. Nunc tempor nulla dui, nec cursus urna dictum at.",
+        images:['./img/projects/turtles/turtle-1.jpg','./img/projects/turtles/turtle-2.jpg']
+    },
+    {
+        title:"Cows",
+        description:"Ut id dui sed sapien sagittis commodo. Ut semper facilisis arcu eu pretium. Vivamus sit amet pharetra erat. Curabitur a sem sed dui accumsan tincidunt. Aliquam felis mi, eleifend at urna vel, laoreet malesuada risus. Morbi pellentesque fringilla purus eu rutrum. Nullam convallis aliquam risus. Donec ex augue, semper et felis vel, tempor posuere erat.",
+        images:['./img/projects/cows/cow-1.jpg','./img/projects/cows/cow-2.jpg']
+    },
+    {
+        title:"Zoo Animals",
+        description:"Duis ultricies feugiat erat eget tempor. Vestibulum bibendum est eu posuere suscipit. Pellentesque tincidunt sapien vitae elit vulputate vestibulum. Proin vel molestie ex. Ut vehicula nibh eu laoreet commodo. Aenean volutpat, nibh in faucibus semper, ipsum est feugiat ex, in volutpat sapien lacus ac massa. Vivamus vel nisi et sapien ullamcorper luctus sit amet sed magna. Sed porttitor sem id eros rhoncus aliquam. Phasellus dapibus, purus a consequat venenatis, nunc diam aliquam tellus, eget congue ante risus a augue. Morbi ut massa ut turpis consequat tincidunt.",
+        images:['./img/projects/zoo/zoo-1.jpg','./img/projects/zoo/zoo-2.jpg']
+    }
+];
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,18 +1,96 @@
 import { gsap } from "gsap";
+import { projects } from "./projects"
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+gsap.registerPlugin(DrawSVGPlugin);
+
 // import { circleAni } from "./circleAnim";
 
-// const mainTL = new gsap.timeline();
+const mainTL = new gsap.timeline();
+
+// window.addEventListener('load', function() {
+
+//     let mainImgArray = document.querySelectorAll('#gallery li');
+
+//     mainImgArray.forEach((heroImg, i) => { 
+//     heroImg.style.backgroundImage = "url(" + projects[i].images[0] + ")";
+//     });
+
+//     let heroLinks = document.querySelectorAll("#gallery a");
+
+//     heroLinks.forEach((link, i) => {
+//         link.addEventListener("click",()=>{
+//             localStorage.setItem("indexValue", i);
+//             link.href = "detail.html?=" + projects[i].title;
+//         });
+//     });
+
+// });
+
+
+window.addEventListener('load', function() {
+
+    // console.log(projects[0].title);
+
+    // populate the hero images
+    let heroImgArray = document.querySelectorAll('#gallery li');
+    // iterate over the heroImgArray and populate the background images
+    heroImgArray.forEach((heroImg, i) => {
+
+        // first pass in the array
+        // heroImg.style.backgroundImage = "url(./img/projects/turtles/turtle-1.jpg)";
+
+        heroImg.style.backgroundImage = "url(" + projects[i].images[0] + ")";
+    });
+
+    let heroLinks = document.querySelectorAll("#gallery a");
+
+    heroLinks.forEach((link, i) => {
+        // console.log("hello");
+
+        link.addEventListener("click", () =>{
+            
+            localStorage.setItem("indexValue", i);
+            link.href = "detail.html?=" + projects[i].title;
+        });
+    });
+})
+
+
+
+// let navButtons = document.querySelectorAll(".nav-btns");
+
+// for (const button of navButtons){
+//     button.addEventListener("click", checkScrolling);
+//     button.addEventListener("click", openCloseMenu);
+// }
+
+// function checkScrolling(e) {
+    
+//     const indexValue = [].indexOf.call(navButtons, e.target)
+//     if (indexValue != -1) {
+//         scrollPage(indexValue);
+//     }
+// }
+
+
+
+
+
 
 // window.addEventListener('load', menuListener);
 window.addEventListener('click', imageRoll);
-
-
+// mainTL.set(".linedown",{drawSVG:"0%"});
+mainTL.set(".linedown",{autoAlpha:0});
 // }
 
 function imageRoll(){
 
-    const rollIn = new gsap.timeline({paused:true});
+    // const rollIn = new gsap.timeline({paused:true});
+    // mainTL.set(".linedown",{autoAlpha:1});
+    mainTL.to(".linedown",{ drawSVG:"100%",duration:1,ease:"power3.inOut"});
+    
 
-    rollIn.to("#section-images",{alpha:0});
-    rollIn.to("#section-images li",{alpha:1,stagger:.2,duration:1,ease:"power3.out",x:0});
+    // rollIn.to("#section-images",{alpha:0});
+    // rollIn.to("image-flex",{alpha:1,stagger:.2,duration:1,ease:"power3.out",x:0});
+    // console.log("fjuck");
 }
